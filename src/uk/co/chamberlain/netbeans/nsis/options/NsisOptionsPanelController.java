9b09eefaec760e0073bf30f4db47dcea3d955d1d
Stephen Chamberlain <steve@steve-chamberlain.co.uk>
2016-10-22 22:54:46 +0200

    NEW: Added initial options page (NSIS icon, primary panel in options dialog, select NSIS home, load/store). Validate: TODO

---
+++ src/uk/co/chamberlain/netbeans/nsis/options/NsisOptionsPanelController.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.co.chamberlain.netbeans.nsis.options;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import javax.swing.JComponent;
+import javax.swing.SwingUtilities;
+import org.netbeans.spi.options.OptionsPanelController;
+import org.openide.util.HelpCtx;
+import org.openide.util.Lookup;
+
+@OptionsPanelController.TopLevelRegistration(
+        categoryName = "#OptionsCategory_Name_Nsis",
+        iconBase = "uk/co/chamberlain/netbeans/nsis/options/nsis32x32.png",
+        keywords = "#OptionsCategory_Keywords_Nsis",
+        keywordsCategory = "Nsis"
+)
+@org.openide.util.NbBundle.Messages({"OptionsCategory_Name_Nsis=NSIS", "OptionsCategory_Keywords_Nsis=NSIS"})
+public final class NsisOptionsPanelController extends OptionsPanelController {
+
+    private NsisPanel panel;
+    private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+    private boolean changed;
+
+    @Override
+    public void update() {
+        getPanel().load();
+        changed = false;
+    }
+
+    @Override
+    public void applyChanges() {
+        SwingUtilities.invokeLater(() -> {
+            getPanel().store();
+            changed = false;
+        });
+    }
+
+    @Override
+    public void cancel() {
+        // need not do anything special, if no changes have been persisted yet
+    }
+
+    @Override
+    public boolean isValid() {
+        return getPanel().valid();
+    }
+
+    @Override
+    public boolean isChanged() {
+        return changed;
+    }
+
+    @Override
+    public HelpCtx getHelpCtx() {
+        return null; // new HelpCtx("...ID") if you have a help set
+    }
+
+    @Override
+    public JComponent getComponent(Lookup masterLookup) {
+        return getPanel();
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener l) {
+        pcs.addPropertyChangeListener(l);
+    }
+
+    @Override
+    public void removePropertyChangeListener(PropertyChangeListener l) {
+        pcs.removePropertyChangeListener(l);
+    }
+
+    private NsisPanel getPanel() {
+        if (panel == null) {
+            panel = new NsisPanel(this);
+        }
+        return panel;
+    }
+
+    void changed() {
+        if (!changed) {
+            changed = true;
+            pcs.firePropertyChange(OptionsPanelController.PROP_CHANGED, false, true);
+        }
+        pcs.firePropertyChange(OptionsPanelController.PROP_VALID, null, null);
+    }
+
+}
