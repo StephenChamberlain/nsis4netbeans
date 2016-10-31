7584f1f27477d95c68b884a84a04b4d1b4503ac7
Stephen Chamberlain <steve@steve-chamberlain.co.uk>
2016-10-31 19:36:43 +0100

    NEW: Added exe file type and icon for compiled installers; double click should start them (todo). Adjusted nsdinc file type to not be in new file list (just want the icon).

--- src/uk/co/chamberlain/netbeans/filetypes/nsdinc/package-info.java
+++ src/uk/co/chamberlain/netbeans/filetypes/nsdinc/package-info.java
@@ -15,7 +15,4 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-@TemplateRegistration(folder = "Other", content = "NsdincTemplate.nsdinc")
 package uk.co.chamberlain.netbeans.filetypes.nsdinc;
-
-import org.netbeans.api.templates.TemplateRegistration;
